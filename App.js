3abc592e8e442b4a238b61f31432424b8d619950
Najuna James <seanjems@gmail.com>
2022-03-24 20:22:57 +0300

    Added Sale Listings screen

--- App.js
+++ App.js
@@ -1,18 +1,8 @@
-//import { StatusBar } from 'expo-status-bar';
-import { View, StatusBar, Text } from "react-native";
 import React from "react";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import Icon from "./app/components/Icon";
 
-import Screen from "./app/components/Screen";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
+import ListingsScreen from "./app/screens/ListingsScreen";
 
 export default function App() {
   // return <WelcomeScreen />;
-  return (
-    <Screen>
-      <AccountScreen />
-    </Screen>
-  );
+  return <ListingsScreen />;
 }
---
+++ app/screens/ListingsScreen.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { View, FlatList, StyleSheet } from "react-native";
+
+import Screen from "../components/Screen";
+import Card from "../components/Card";
+import colors from "../config/colors";
+
+const myItems = [
+  {
+    id: 1,
+    title: "Red jacket for sale",
+    price: 100,
+    image: require("../assets/jacket.jpg"),
+  },
+  {
+    id: 2,
+    title: "Couch in perfect condition",
+    price: 6000,
+    image: require("../assets/couch.jpg"),
+  },
+];
+
+function ListingsScreen(props) {
+  return (
+    <Screen style={styles.screen}>
+      <View style={styles.container}>
+        <FlatList
+          data={myItems}
+          keyExtractor={(listing) => listing.id.toString()}
+          renderItem={({ item }) => (
+            <Card
+              title={item.title}
+              subTitle={"$" + item.price}
+              image={item.image}
+            />
+          )}
+        />
+      </View>
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 10,
+  },
+  screen: {
+    flex: 1,
+    backgroundColor: colors.light,
+  },
+});
+export default ListingsScreen;
